fb1d2d3ad3e41ac1a989cea9524c741b530172c5
Tobias Stolzmann <tobias.stolzmann@potassco.com>
2024-08-09 09:06:10 +0200

    TODO

--- src/verifying/proof/mod.rs
+++ src/verifying/proof/mod.rs
@@ -1,28 +1,42 @@
-use {crate::verifying::problem::Problem, indexmap::IndexMap};
+use {
+    crate::verifying::problem::Problem,
+    indexmap::IndexMap,
+    std::fmt::{Debug, Display},
+};
 
 pub mod vampire;
 
-pub struct Outcome;
+pub enum Status {
+    // TODO: SZS status ontology
+}
+
+pub trait Outcome: Display + Debug + Clone {
+    fn status(&self) -> Option<Status>;
+}
 
 pub trait Prover {
-    fn prove(&self, problem: &Problem) -> Outcome;
+    type Outcome: Outcome;
+    fn prove(&self, problem: &Problem) -> Self::Outcome;
 }
 
-pub struct Report {
-    pub problems: IndexMap<Problem, Outcome>,
+pub struct Report<O: Outcome> {
+    pub problems: IndexMap<Problem, O>,
 }
 
-impl Report {
+impl<O: Outcome> Report<O> {
     pub fn generate_from(
         problems: impl IntoIterator<Item = Problem>,
-        prover: impl Prover,
+        prover: impl Prover<Outcome = O>,
         // threads: usize,
     ) -> Self {
         Report {
-            problems: problems.into_iter().map(|problem| {
-                let outcome = prover.prove(&problem);
-                (problem, outcome)
-            }).collect()
+            problems: problems
+                .into_iter()
+                .map(|problem| {
+                    let outcome = prover.prove(&problem);
+                    (problem, outcome)
+                })
+                .collect(),
         }
     }
 }
--- src/verifying/proof/vampire.rs
+++ src/verifying/proof/vampire.rs
@@ -1,9 +1,54 @@
-use {crate::verifying::{problem::{Problem}, proof::{Prover, Outcome}}};
+use {
+    crate::verifying::{
+        problem::Problem,
+        proof::{Outcome, Prover, Status},
+    },
+    std::{
+        fmt::Display,
+        io::Write as _,
+        process::{Command, Stdio},
+    },
+};
+
+#[derive(Debug, Clone)]
+pub enum VampireOutcome {
+    UnableToSpawn,
+    UnableToOpenStdin,
+    UnableToWriteToStdin,
+    UnableToWait,
+    SuccessfulExecution
+}
+
+impl Outcome for VampireOutcome {
+    fn status(&self) -> Option<Status> {
+        todo!()
+    }
+}
+
+impl Display for VampireOutcome {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        todo!()
+    }
+}
 
 pub struct Vampire;
 
 impl Prover for Vampire {
-    fn prove(&self, problem: &Problem) -> Outcome {
+    type Outcome = VampireOutcome;
+
+    fn prove(&self, problem: &Problem) -> VampireOutcome {
+        let mut child = Command::new("vampire")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .stderr(Stdio::piped())
+            .spawn()
+            .unwrap(); // TODO: Proper error handling
+
+        let stdin = child.stdin.as_mut().unwrap(); // TODO: Proper error handling
+        write!(stdin, "{problem}").unwrap(); // TODO: Proper error handling
+
+        let output = child.wait_with_output().unwrap(); // TODO: Proper error handling
+
         todo!()
     }
 }
